61cb35284352f2d93543cc2cd0a0c28882444c9a
Paloma Vilchis <paloma.vilchis@gmail.com>
2021-07-22 23:02:14 -0500

    ColabaracionG4

--- print("Hello world").py
+++ print("Hello world").py
@@ -1,4 +1,6 @@
-import GPIO.rfi as GPIO
+import GPIO.RFi as GPIO
 pin = 40
 print("Hello world")
+printf("Luis Angel Sánchez Hernández")
+print("Luis Angel Sánchez Hernández")
 print("Luis Angel Sánchez Hernández")
