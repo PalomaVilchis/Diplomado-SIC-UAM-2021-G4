b8b659c3785602302f86ec17c9ac851e617410bf
CarlosATG <87503410+CarlosATG@users.noreply.github.com>
2021-07-24 11:31:21 -0500

    de neuvo

--- CarlosTG_Program.py
+++ CarlosTG_Program.py
@@ -8,5 +8,5 @@ user_dias =  total_dias - (int (edad) * 365)
 user_semanas = total_semanas - int (edad) * 52
 user_meses = total_meses - int (edad) * 12
 #Datos a imprimir
-print(f"te quedan {user_dias} dias, {user_dias} semanas y {user_dias} meses por vivir")
+print(f"te quedan {user_dias} dias, {user_semanas} semanas y {user_meses} meses por vivir")
 print("Considerando que vas a vivir hasta los 90")
