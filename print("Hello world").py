21c761203361ca2ebd0f0f6d6a0903e61b7062f8
CristianPlaz <capc@azc.uam.mx>
2021-07-24 11:22:10 -0500

    commit 2 240721

--- print("Hello world").py
+++ print("Hello world").py
@@ -1,3 +1,3 @@
 import GPIO.rfi as GPIO
 pin = 40
-print("el universo es una creación mental sostenida en la mente del todoo")
+print("el universo es una creación mental sostenida en la mente del todo")
