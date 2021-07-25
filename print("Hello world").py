365bfec9f52b528665fae749de48be7fdbb6a6cf
MarlenMoran <mmc@azc.uam.mx>
2021-07-24 23:02:02 -0500

    MarlenMoranCamero3

--- print("Hello world").py
+++ print("Hello world").py
@@ -1,10 +1,5 @@
 import GPIO.rfi as GPIO
 pin = 40
-<<<<<<< HEAD
 
 print("Hello world")
-print("Marlen Moran Camero.")
-=======
-print("Hello world")
-print("Marlen Moran Camero--")
->>>>>>> a9c0e34cf534e213875a177688963dff1f51e989
+print("Marlen Moran Camero")
